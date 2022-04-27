0a0138b16c07fef33b88da49dae1c57bc4663a61
Lautaro <77054024+yanuzzio@users.noreply.github.com>
2022-04-27 17:10:56 +0200

    Practica1

---
+++ Proyect/_Scripts/ItemsRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ItemsRotation : MonoBehaviour
+{
+    //Rotación de los objectos
+
+    public float speedRotationY;
+
+    void Update()
+    {
+        transform.Rotate(0, speedRotationY, 0);
+    }
+}
---
+++ Proyect/_Scripts/Player/InterfaceController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceController : MonoBehaviour
+{
+    public PlayerController playerController;
+    public Text interfaceText;
+    public Text interfaceControllText;
+    public Text pausedText;
+    bool bInterfaceGame;
+    bool bPauseGame;
+
+    void Update()
+    {
+        InterfaceGame();
+
+        GamePause();
+    }
+
+    void InterfaceGame() //Función para mostrar interface en pantalla
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            interfaceText.gameObject.SetActive(false);
+            bInterfaceGame = !bInterfaceGame;
+        }
+
+        if (bInterfaceGame) interfaceControllText.gameObject.SetActive(true);
+        else interfaceControllText.gameObject.SetActive(false);
+    }
+
+    void GamePause() //Función para pausar el juego
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            bPauseGame = !bPauseGame;
+        }
+
+        if(bPauseGame)
+        {
+            Time.timeScale = 0;
+            pausedText.gameObject.SetActive(true);
+            playerController.gameEnable = false;  
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pausedText.gameObject.SetActive(false);
+            playerController.gameEnable = true;
+        }
+    }
+}
